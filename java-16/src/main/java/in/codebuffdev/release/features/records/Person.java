6368494970cf9ed18e29e51f663836cc6bb3887a
codebuffdev <codebuffdev@gmail.com>
2024-12-10 19:39:23 +0530

    Java 16 empty maven project

---
+++ java-16/src/main/java/in/codebuffdev/release/features/records/Person.java
@@ -0,0 +1,7 @@
+package in.codebuffdev.release.features.records;
+
+// concise constructor
+
+public record Person(int age, String name) {
+}
+
---
+++ java-16/src/main/java/in/codebuffdev/release/features/records/Test.java
@@ -0,0 +1,11 @@
+package in.codebuffdev.release.features.records;
+
+public class Test {
+    public static void main(String[] args) {
+        Person person = new Person(10, "sp");
+
+        //
+        // Person person2 = new Person();
+        System.out.println("person = " + person);
+    }
+}
