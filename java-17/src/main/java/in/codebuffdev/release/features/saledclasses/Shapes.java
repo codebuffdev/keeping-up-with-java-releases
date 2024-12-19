483a79d67ef124b16e3162e91d65fc3d21887b8a
codebuffdev <codebuffdev@gmail.com>
2024-12-19 20:26:30 +0530

    Java 17 sealed classes features

--- java-17/src/main/java/in/codebuffdev/release/features/saledclasses/Shapes.java
+++ java-17/src/main/java/in/codebuffdev/release/features/saledclasses/Shapes.java
@@ -1,5 +1,11 @@
 package in.codebuffdev.release.features.saledclasses;
 
+/**
+ * sealed class
+ * java-15 : as preview feature
+ * java-17 : as standard feature
+ */
+
 public abstract sealed class Shapes permits Circle, Rectangle {
     // Circle and Rectangle classes will be able to extend Shapes class
 }
